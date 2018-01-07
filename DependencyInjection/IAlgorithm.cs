c175217f6e98bbb2cb5c333e9b33e2a3be78bd98
Вадим Шванов <adm.shwan@gmail.com>
2018-01-07 19:57:27 +0300

    Добавьте файлы проекта.

---
+++ DependencyInjection/IAlgorithm.cs
@@ -0,0 +1,14 @@
+﻿namespace DependencyInjection
+{
+    /// <summary>
+    /// Базовый интерфейс криптоалгорима хеш-функции.
+    /// </summary>
+    public interface IAlgorithm 
+    {
+        /// <summary>
+        /// Вычисление нового хеша.
+        /// </summary>
+        /// <returns>Успешность нахождения хеша.</returns>
+        bool Hash();
+    }
+}
